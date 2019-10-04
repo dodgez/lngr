7e70219223981a51aa8dd8e2e07d0b33573611b7
dodgez <zacharysdodge@gmail.com>
2019-10-04 10:17:11 -0700

    Add parser

--- src/utils.js
+++ src/utils.js
@@ -20,6 +20,52 @@ class StringStream {
   }
 }
 
+class TokenStream {
+  tokens;
+  index;
+
+  constructor(tokens) {
+    this.tokens = tokens;
+    this.index = 0;
+  }
+
+  matchesToken(token_type) {
+    if (this.isEOF()) return false;
+
+    return this.tokens[this.index].type == token_type;
+  }
+
+  peekToken() {
+    return this.tokens[this.index].token;
+  }
+
+  advance() {
+    this.index++;
+  }
+
+  isEOF() {
+    return this.index >= this.tokens.length;
+  }
+}
+
+class ASTNode {
+  type;
+  children;
+  token;
+
+  constructor(type, children, token = null) {
+    this.type = type;
+    this.children = children;
+    this.token = token;
+  }
+}
+
+module.exports.ASTNode = ASTNode;
+
 module.exports.getStringStream = function(string) {
   return new StringStream(string);
+}
+
+module.exports.getTokenStream = function(tokens) {
+  return new TokenStream(tokens);
 }

62a0c74ee031f4f56cb9f64120bf1f9874b1e391
dodgez <zacharysdodge@gmail.com>
2019-10-04 11:02:45 -0700

    Remove broken OR rules

--- src/parser.js
+++ src/parser.js
@@ -7,39 +7,38 @@ module.exports.formatRules = function(raw_rules) {
 
   rules = rules.map(rule => {
     rule.parse = function(stream) {
-      let options = rule.expr.split('|');
-      for (let option of options.map(option => option.trim())) {
-        let exprs = option.split(' ');
-        let children = [];
-        let passed = true;
+      let exprs = rule.expr.split(' ');
+      let children = [];
+      let passed = true;
 
-        for (let expr of exprs) {
-          let is_token = expr.match(/^[A-Z]/) ? true : false;
-          
-          if (is_token) {
-            if (stream.matchesToken(expr)) {
-              children.push(new utils.ASTNode(expr, null, stream.peekToken()));
-              stream.advance()
-            } else {
-              passed = false;
-              break;
-            }
+      for (let expr of exprs) {
+        let is_token = expr.match(/^[A-Z]/) ? true : false;
+        
+        if (is_token) {
+          if (stream.matchesToken(expr)) {
+            children.push(new utils.ASTNode(expr, null, stream.peekToken()));
+            stream.advance()
           } else {
-            let node = rules.filter(rule => rule.name == expr)[0].parse(stream);
+            passed = false;
+            break;
+          }
+        } else {
+          let node = rules.filter(rule => rule.name == expr)[0].parse(stream);
 
-            if (node) {
-              children.push(node);
-            } else {
-              passed = false;
-              break;
-            }
+          if (node) {
+            children.push(node);
+          } else {
+            passed = false;
+            break;
           }
         }
-
-        if (passed) return new utils.ASTNode(rule.name, children);
       }
 
-      return null;
+      if (passed) {
+        return new utils.ASTNode(rule.name, children);
+      } else {
+        return null;
+      }
     }
 
     return rule;
